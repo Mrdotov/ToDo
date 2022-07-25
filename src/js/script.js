975082fa8e58d099f5ddaf8f249830042a0181a6
Mrdotov <valdom213@gmail.com>
2022-07-25 10:44:08 +0300

    ToDo

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,32 @@
-alert('say hello')
+window.onload = function() {
+
+    var todoList = [];
+
+    document.getElementById('add').onclick = function() {
+
+        var d = document.getElementById('in').value;
+        // {todo : Добавить хлеб , check : false   }
+        var temp = {};
+        temp.todo = d;
+        temp.check = false;
+        var i = todoList.length;
+        todoList[i] = temp;
+        console.log(todoList);
+        out();
+        localStorage.setItem('todo', JSON.stringify(todoList))
+    }
+
+    function out() {
+        var out = '';
+        for (var key in todoList) {
+            out += todoList[key].todo + '<br>';
+            if (todoList[key].check == true) {
+                out += '<input type="checkbox" checked>';
+            } else {
+                out += '<input type="checkbox" >';
+            }
+
+        }
+        document.getElementById('out').innerHTML = out;
+    }
+}
